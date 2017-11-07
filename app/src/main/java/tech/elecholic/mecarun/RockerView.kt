8a07dbe7b249afd16ca30f45874ce134b72237ac
RkTeddy <838474413@qq.com>
2017-11-07 17:29:12 +0800

    Parameters defined

--- app/src/main/java/tech/elecholic/mecarun/RockerView.kt
+++ app/src/main/java/tech/elecholic/mecarun/RockerView.kt
@@ -12,4 +12,21 @@ import tech.elecholic.mecarun.R
 
 class RockerView: View {
 
+    private var innerColor: Int
+    private var outerColor: Int
+    private var realWidth = 0f
+    private var realHeight = 0f
+    private var innerCircleX = 0f
+    private var innerCircleY = 0f
+    private var outerRadius = 0f
+    private var innerRadius = 0f
+    private var angle = 0f
+    private var outerCircle: Paint
+    private var innerCircle: Paint
+    private lateinit var mListener: OnAngleChangedListener
+    private var OUTER_WIDTH_SIZE: Int
+    private var OUTER_HEIGHT_SIZE: Int
+    private val INNER_COLOR_DEFAULT = Color.parseColor("#d32f2f")
+    private val OUTER_COLOR_DEFAULT = Color.parseColor("#f44336")
+
 }
